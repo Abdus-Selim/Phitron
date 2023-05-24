a7a894f9edc847c31c8c3a68a80edd1ee78dd147
Abdus Selim <cse.selim.mu22@gmail.com>
2023-05-24 14:10:08 +0600

    Dubly_linkedlist done

--- C_plus_plus/Phitron_DS/Dubly_linkedlist.cpp
+++ C_plus_plus/Phitron_DS/Dubly_linkedlist.cpp
@@ -19,7 +19,7 @@ public:
         sz = 0;
     }
     /*Creates a new node with the given data*/
-    node *CreateNewNode(int data)
+    node *CreateNewNode(int data) // O(1)
     {
         node *newnode = new node;
         newnode->data = data;
@@ -28,7 +28,7 @@ public:
         return newnode;
     }
     /*Inserts a newnode with the given data into the head*/
-    void InsertAtHead(int data)
+    void InsertAtHead(int data) // O(1)
     {
         sz++;
         node *newnode = CreateNewNode(data);
@@ -52,6 +52,87 @@ public:
         }
         cout << endl;
     }
+    /*Insert a vlaue at any index*/
+    void InsertAtAnyIndex(int index, int data) // O(n)
+    {
+        if (index > sz)
+        {
+            return;
+        }
+        if (index == 0)
+        {
+            InsertAtHead(data);
+            return;
+        }
+        node *a = head;
+        int cur_index = 0;
+        while (cur_index != index - 1)
+        {
+            a = a->nxt;
+            cur_index++;
+        }
+        node *newnode = CreateNewNode(data);
+        newnode->nxt = a->nxt;
+        newnode->prv = a;
+        node *b = a->nxt;
+        b->prv = newnode;
+        a->nxt = newnode;
+        sz++;
+    }
+    /*Delete any index*/
+    void DeleteAIndex(int index) // O(n)
+    {
+        if (index >= sz)
+        {
+            cout << "Index does not exist!" << endl;
+        }
+        node *a = head;
+        int cur_index = 0;
+        while (cur_index != index)
+        {
+            a = a->nxt;
+            cur_index++;
+        }
+        node *b = a->prv;
+        node *c = a->nxt;
+        if (b != NULL)
+        {
+            b->nxt = c;
+        }
+        if (c != NULL)
+        {
+            c->prv = b;
+        }
+        delete a;
+        if (index == 0)
+        {
+            head = c;
+        }
+        sz--;
+    }
+    /*Reverse the dubly linkedlist*/
+    void Reverse() // O(n)
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        node *a = head;
+        int cur_index = 0;
+        while (cur_index != sz - 1)
+        {
+            a = a->nxt;
+            cur_index++;
+        }
+        // Now last index is in a
+        node *b = head;
+        while (b != NULL)
+        {
+            swap(b->nxt, b->prv);
+            b = b->prv;
+        }
+        head = a;
+    }
     int getsize()
     {
         return sz;
@@ -68,5 +149,17 @@ int main()
 
     L.Traverse();
     cout << L.getsize() << endl;
+
+    L.InsertAtAnyIndex(2, 100);
+    L.Traverse();
+    cout << L.getsize() << endl;
+    L.DeleteAIndex(0);
+    L.Traverse();
+    cout << L.getsize() << endl;
+
+    L.Reverse();
+    L.Traverse();
+    cout << L.getsize() << endl;
+
     return 0;
 }
