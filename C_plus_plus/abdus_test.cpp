a77e65edd41b65b7393674d6ecb61c2b8ac242d6
Abdus Selim <cse.selim.mu22@gmail.com>
2023-06-26 01:44:53 +0600

    TEST

---
+++ C_plus_plus/abdus_test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int x;
+    float y;
+    double z;
+
+    cin >> x >> y >> z;
+
+    double sum = x + y + z;
+
+    cout << x << "\n"
+         << y << "\n"
+         << z << "\n";
+    cout << "Here is the sum " << sum;
+    return 0;
+} 
