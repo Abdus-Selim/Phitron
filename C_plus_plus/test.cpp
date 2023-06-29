432fde62dfdfa1ca35c712b017cc16348657f491
Abdus Selim <cse.selim.mu22@gmail.com>
2023-06-29 22:33:06 +0600

    Ds lab week 5

--- C_plus_plus/test.cpp
+++ C_plus_plus/test.cpp
@@ -1,14 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int i = 0;
-int main()
-{
-    if (i == 100)
-    {
-        return 0;
-    }
-    cout<<++i<<endl;
-    main();
+int main() {
+  if (i == 100) {
+    return 0;
+  }
+  cout << ++i << endl;
+  main();
 }
