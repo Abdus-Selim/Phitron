06c859990a2c1f3044a98730d755eb606d31596c
Abdus Selim <cse.selim.mu22@gmail.com>
2023-07-06 22:32:06 +0600

    TEST

---
+++ C_plus_plus/Phitron_DS/Week 6/stack1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack
+{
+public:
+    const int max_size = 500;
+
+    int a[max_size];
+    int stack_size;
+    Stack
+    {
+        stack_size = 0;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
