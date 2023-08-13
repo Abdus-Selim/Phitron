e378870bab7e038e3f6f895e1e57dd6a1abc5a31
Abdus Selim <cse.selim.mu22@gmail.com>
2023-08-13 21:11:04 +0600

    BST update: Implement Delete Function

--- Phitron_DS/Week 8/Binary Search Tree/BST.cpp
+++ Phitron_DS/Week 8/Binary Search Tree/BST.cpp
@@ -105,6 +105,84 @@ public:
         }
         return false;
     }
+
+    void Delete(int value)
+    {
+        node *Parent = root;
+        node *prv = NULL;
+
+        while (Parent != NULL)
+        {
+            if (value > Parent->value)
+            {
+                prv = Parent;
+                Parent = Parent->Right;
+            }
+            else if (value < Parent->value)
+            {
+                prv = Parent;
+                Parent = Parent->Left;
+            }
+            else
+                break;
+        }
+        if (Parent == NULL)
+        {
+            cout << "Value is not present the BST!" << endl;
+            return;
+        }
+        // case 1: both child is NULL
+        if (Parent->Left == NULL && Parent->Right == NULL)
+        {
+            if (prv->Left != NULL && prv->Left->value == Parent->value)
+            {
+                prv->Left = NULL;
+            }
+            else
+            {
+                prv->Right = NULL;
+            }
+            delete Parent;
+            return;
+        }
+        // Case 2: node has only one child;
+        if (Parent->Left == NULL && Parent->Right != NULL)
+        {
+            if (prv->Left != NULL && prv->Left->value == Parent->value)
+            {
+                prv->Left = Parent->Left;
+            }
+            else
+            {
+                prv->Right = Parent->Right;
+            }
+            delete Parent;
+            return;
+        }
+        if (Parent->Left != NULL && Parent->Right == NULL)
+        {
+            if (prv->Left != NULL && prv->Left->value == Parent->value)
+            {
+                prv->Left = Parent->Left;
+            }
+            else
+            {
+                prv->Right = Parent->Right;
+            }
+            delete Parent;
+            return;
+        }
+
+        //Case 3: node has two child
+        node *tmp = Parent->Right;
+        while(tmp->Left != NULL)
+        {
+            tmp = tmp->Left;
+        }
+        int saved = tmp->value;
+        Delete(saved);
+        Parent->value = saved;
+    }
 };
 
 int main()
@@ -116,19 +194,22 @@ int main()
     b.Insert(5);
     b.Insert(7);
     b.Insert(8);
-    // b.BFS();
-    int num;
-    while (cin >> num)
-    {
-        if (b.Search(num))
-        {
-            cout << num << " is existing in the BST!" << endl;
-        }
-        else
-        {
-            cout << num << " doesn't exist in the BST!" << endl;
-        }
-    }
+
+    
+    b.Delete(6);
+    b.BFS();
+    // int num;
+    // while (cin >> num)
+    // {
+    //     if (b.Search(num))
+    //     {
+    //         cout << num << " is existing in the BST!" << endl;
+    //     }
+    //     else
+    //     {
+    //         cout << num << " doesn't exist in the BST!" << endl;
+    //     }
+    // }
     // cout<<b.Search(6)<<endl;
     // cout<<b.Search(10)<<endl;
 
