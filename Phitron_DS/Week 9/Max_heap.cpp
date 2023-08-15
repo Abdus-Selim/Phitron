e1a804f45fe0461ccdc4ca9367bef06353ea0a48
Abdus Selim <cse.selim.mu22@gmail.com>
2023-08-15 21:44:02 +0600

    Max Heap update: Add Delete Function

--- Phitron_DS/Week 9/Max_heap.cpp
+++ Phitron_DS/Week 9/Max_heap.cpp
@@ -9,6 +9,7 @@ public:
     MaxHeap()
     {
     }
+    // O(logn)
     void up_heapify(int idx)
     {
         while (idx > 0 && nodes[idx] > nodes[(idx - 1) / 2])
@@ -17,12 +18,47 @@ public:
             idx = (idx - 1) / 2;
         }
     }
+
+    // O(logn)
+    void down_heapify(int idx)
+    {
+        while (1)
+        {
+            int largest = idx;
+            int l = 2 * idx + 1;
+            int r = 2 * idx + 2;
+            if (l < nodes.size() && nodes[largest] < nodes[l])
+            {
+                largest = l;
+            }
+            if (r < nodes.size() && nodes[largest] < nodes[r])
+            {
+                largest = r;
+            }
+            if (largest == idx)
+            {
+                break;
+            }
+            swap(nodes[idx], nodes[largest]);
+            idx = largest;
+        }
+    }
+
+    // O(logn)
+    void Delete(int idx)
+    {
+        swap(nodes[idx], nodes[nodes.size() - 1]);
+        nodes.pop_back();
+        down_heapify(idx);
+    }
+    // O(logn)
     void Insert(int x)
     {
         nodes.push_back(x);
         up_heapify(nodes.size() - 1);
     }
 
+    // O(n)
     void PrintHeap()
     {
         for (int i = 0; i < nodes.size(); i++)
@@ -41,6 +77,11 @@ int main()
     heap.Insert(9);
     heap.Insert(1);
     heap.Insert(10);
+    heap.Insert(20);
+    heap.Insert(30);
+    heap.PrintHeap();
+
+    heap.Delete(0);
 
     heap.PrintHeap();
 
