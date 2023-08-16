f97cf6b5a054d42b908b29096bf091d0a1723899
Abdus Selim <cse.selim.mu22@gmail.com>
2023-08-16 20:07:16 +0600

    STL pair

---
+++ Phitron_DS/Week 9/pair.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    pair<string, int> a;
+    a.first = "abcd";
+    a.second = 6;
+    cout << "Pair a = " << a.first << ", " << a.second << endl;
+
+    pair<int, int> b;
+    b.first = 600;
+    b.second = 300;
+    cout << "Pair b = " << b.first << ", " << b.second << endl;
+    return 0;
+}
